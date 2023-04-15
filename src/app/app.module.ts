37334670c9fa6a1dc558bc16b6e43589f98b0364
hsouna23 <hassanabdelkafi1@gmail.com>
2023-04-15 14:50:00 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { WeatherComponent } from './weather/weather.component';
+import {RouterModule} from '@angular/router';
+import {allAppRoutes} from './routes';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(allAppRoutes),
+    AppRoutingModule,
+        ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
